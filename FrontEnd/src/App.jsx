675a679695beafb8c7e9c92e7d3bf8aaae2b86bb
Reaper23 <pankajhlwt@gmail.com>
2025-09-14 00:10:37 +0530

    a checkpoint

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,8 @@
+import { useState } from 'react'
+import {LoadInterface} from './components/Interface.jsx'
+
+function App() {
+  return (<LoadInterface />)
+}
+
+export default App
---
+++ FrontEnd/src/components/Interface.jsx
@@ -0,0 +1,26 @@
+import './style/interface.css'
+import { RefreshCw, Sparkles } from "lucide-react";
+
+
+export function LoadInterface(){
+    return (
+    <>
+     {/* -- Header -- */}
+    <div class="header">
+        <div class="header-left"> 
+            <Sparkles /> <h3>. . Chatter</h3>
+        </div>
+        <div class="header-right">
+             <button class="new-chat-button"> <RefreshCw /> New chat</button>
+        </div>
+    </div>
+    <div class="chat-container">
+    
+       
+    </div>
+    <div class="input">
+       
+    </div>
+    </>
+    );
+}
