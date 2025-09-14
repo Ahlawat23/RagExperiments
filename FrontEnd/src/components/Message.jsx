9ac3fa92ea36a59c2a3f9e250161582a6f852644
Reaper23 <pankajhlwt@gmail.com>
2025-09-14 14:29:45 +0530

    minor

--- FrontEnd/src/components/Message.jsx
+++ FrontEnd/src/components/Message.jsx
@@ -1,10 +1,12 @@
 import { User, Bot } from "lucide-react";
+import './style/message.css'
 
 export function MessageUI({message, isUser, timestamp}){
     return(
         <>
         <div class="messageBox">
-            
+            <div class="message-text">{message}</div>
+            <div class="message-timestamp">{timestamp}</div>
         </div>
         </>
     );
